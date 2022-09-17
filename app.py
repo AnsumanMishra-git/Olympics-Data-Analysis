33af98554ab5efcb7fd7acb51f0ceb72fd57a3e1
Ansuman Mishra <56594734+AnsumanMishra-git@users.noreply.github.com>
2022-09-17 22:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,8 @@ region_df = pd.read_csv('noc_regions.csv')
 df = preprocessor.preprocess(df,region_df)
 
 st.sidebar.title("Olympics Analysis")
-st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
-user_menu = st.sidebar.radio(
-    'Select an Option',
-    ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
+st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png',use_column_width=True)
+user_menu = st.sidebar.radio('Select an Option',('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
 )
 
 if user_menu == 'Medal Tally':
